bc04b69c5e646475e421d2f2373c3501533f9b0c
fan <fan@120.79.143.213:29418>
2021-05-15 16:43:00 +0800

    修改

---
+++ heghStu/hgh-user/hgh-user-service/src/test/java/com.hgh/ProtoBufferTest.java
@@ -0,0 +1,26 @@
+package com.hgh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ProtoBufferTest
+ *
+ * @author lf
+ * @date 2021/5/15
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ProtoBufferTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello Spring.");
+    }
+}
---
+++ heghStu/pb/gen.bat
@@ -0,0 +1,11 @@
+
+@echo off
+pushd "%cd%"
+cd ..
+set pard=%cd%
+popd
+echo parentPath: %pard%
+echo curPath: %cd%
+
+%cd%/protoc.exe  --proto_path=%cd%/ *.proto  --java_out=%pard%\hgh-common\src\main\java\com\hgh\entity
+pause
