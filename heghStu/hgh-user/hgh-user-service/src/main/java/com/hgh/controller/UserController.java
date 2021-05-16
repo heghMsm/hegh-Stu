7a88dc70bd28790036575fd375c369e8fbff9eb6
fan <fan@120.79.143.213:29418>
2021-05-16 19:04:19 +0800

    protobuffer数据载体案例

--- heghStu/hgh-user/hgh-user-service/src/main/java/com/hgh/controller/UserController.java
+++ heghStu/hgh-user/hgh-user-service/src/main/java/com/hgh/controller/UserController.java
@@ -1,19 +1,29 @@
 package com.hgh.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.hgh.entity.po.User;
-import com.hgh.service.IUserService;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hgh.entity.User;
 import com.hgh.entity.dto.Result;
+import com.hgh.service.IUserService;
 import com.hgh.utils.ResultUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import pb.Package;
 
 /**
  * @author hegh
  * @date 2021-05-15
  */
-@RestController("/user")
+@RestController
+@RequestMapping("user")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -24,5 +34,16 @@ public class UserController {
         Page<User> page = new Page<>(1,10);
         Page<User> userPage = userService.page(page);
         return ResultUtil.success(userPage);
+    }
+
+
+    @PostMapping(value = "/test")
+    public byte[] test(@RequestBody  byte[] data) throws InvalidProtocolBufferException {
+        Package.Pack pack = Package.Pack.parseFrom(data);
+        log.info(pack.toString());
+
+        Package.Pack.Builder builder = Package.Pack.newBuilder();
+        builder.setContext(100);
+        return builder.build().toByteArray();
     }
 }
--- heghStu/hgh-user/hgh-user-service/src/test/java/com.hgh/ProtoBufferTest.java
+++ heghStu/hgh-user/hgh-user-service/src/test/java/com.hgh/ProtoBufferTest.java
@@ -1,10 +1,16 @@
 package com.hgh;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,6 +27,23 @@ public class ProtoBufferTest {
 
     @Test
     public void test() {
+        pb.Package.Pack.Builder builder = pb.Package.Pack.newBuilder();
+        builder.setContext(200);
         System.out.println("Hello Spring.");
+        byte[] bytes = builder.build().toByteArray();
+        log.info(bytes.toString());
+
+        try{
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            URI uri = new URI("http", null, "127.0.0.1", 8001, "/user/test", "", null);
+            HttpPost post = new HttpPost(uri);
+            post.setEntity(new ByteArrayEntity(bytes));
+            HttpResponse response = httpClient.execute(post);
+
+            log.info("=========");
+        }catch (Exception e){
+
+        }
+
     }
 }
